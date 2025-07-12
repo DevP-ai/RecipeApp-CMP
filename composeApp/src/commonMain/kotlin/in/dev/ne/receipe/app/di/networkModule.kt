f91d326bee99b4859faa00582cb920a311a88322
Devajit <patar1447@gmail.com>
2025-07-13 00:44:57 +0530

    HttpClient implemented

--- composeApp/src/commonMain/kotlin/in/dev/ne/receipe/app/di/networkModule.kt
+++ composeApp/src/commonMain/kotlin/in/dev/ne/receipe/app/di/networkModule.kt
@@ -1,7 +1,11 @@
 package `in`.dev.ne.receipe.app.di
 
+import `in`.dev.ne.receipe.app.features.common.data.api.httpClient
+import io.ktor.client.HttpClient
 import org.koin.dsl.module
 
 fun networkModule() = module {
-
+    single <HttpClient>{
+        httpClient
+    }
 }
