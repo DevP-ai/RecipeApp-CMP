a09c8dffd5b8432b353f185b8a74ca27470b0f14
Devajit <patar1447@gmail.com>
2025-07-12 23:53:22 +0530

    All the required module setup

---
+++ composeApp/src/commonMain/kotlin/in/dev/ne/receipe/app/di/Koin.kt
@@ -0,0 +1,13 @@
+package `in`.dev.ne.receipe.app.di
+
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.KoinAppDeclaration
+
+fun initKoin(
+    additionalModule: List<Module> = emptyList(),
+    appDeclaration: KoinAppDeclaration = {}
+)= startKoin {
+    appDeclaration()
+    modules(additionalModule+cacheModule()+dataModule()+networkModule()+viewModelModule())
+}
---
+++ composeApp/src/commonMain/kotlin/in/dev/ne/receipe/app/di/dataModule.kt
@@ -0,0 +1,7 @@
+package `in`.dev.ne.receipe.app.di
+
+import org.koin.dsl.module
+
+fun dataModule() = module {
+
+}
